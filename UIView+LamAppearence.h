76bb3170547661652ce19ca4b85b7be360df59a7
atouze <adeline@atelierdumobile.com>
2014-05-14 16:49:54 +0200

    UIView+LamAppearence :  Add surface, shadow, wiggleView, setFontSizeForSubViews methods

--- UIView+LamAppearence.h
+++ UIView+LamAppearence.h
@@ -10,12 +10,14 @@
 #import <UIKit/UIkit.h>
 
 
-
 /// Modification of the view's appearance.
 @interface UIView (LamAppearence)
 
 /// Generate an image of the receiver appearance.
 - (UIImage *)generateImage;
+
+/// Returns the surface of the view.
+- (CGFloat)surface;
 
 /// Round the corners of the view.
 /// @param aRadius Roundness of the view's corner.
@@ -25,6 +27,17 @@
 /// @param aColor Color of the border.
 /// @param aWidth Width of the border.
 - (void)addBorderWithColor:(UIColor *)aColor width:(CGFloat)aWidth;
+
+/// Add a shadow to the view.
+/// Reasonable values would be:
+///     [self.searchTxtFieldView addShadowWithOpacity:0.5 blur:3 offset:CGSizeMake(0, -3)];
+- (void)addShadowWithOpacity:(CGFloat)anOpacity blur:(CGFloat)aBlurRadius offset:(CGSize)anOffset;
+
+/// Animate a view to make it dance.
+/// @todo Move this somewhere else.
+/// @param intensity Intensity of the dance.
+/// @param duration Duration of the dance. At the end of this duration, the dance stops.
+- (void)wiggleViewWithIntensity:(CGFloat)intensity andDuration:(CGFloat)duration;
 
 /// Apply the given font to all subviews, but keeps the subview's current font size.
 /// @param aFontName The name of the font to apply to all subviews.
@@ -36,5 +49,20 @@
 /// @param aBoldFontName The name of the font to apply to all subviews, when the original font is bold.
 - (void)setFontForAllSubviewsWithName:(NSString *)aFontName
                          boldFontName:(NSString *)aBoldFontName;
+
+/// Apply the given font to all subviews, and set the subview's new font size.
+/// @param aFontName The name of the font to apply to all subviews.
+/// @param fontSize The size of the font to apply to all subviews.
+- (void)setFontForAllSubviewsWithName:(NSString *)aFontName
+							  andSize:(CGFloat)fontSize;
+
+/// Apply the given font to all subviews, and set the subview's new font size.
+/// If the original font was bold, boldFontName is used instead.
+/// @param aFontName The name of the font to apply to all subviews.
+/// @param aBoldFontName The name of the font to apply to all subviews, when the original font is bold.
+/// @param fontSize The size of the font to apply to all subviews.
+- (void)setFontForAllSubviewsWithName:(NSString *)aFontName
+                         boldFontName:(NSString *)aBoldFontName
+							  andSize:(CGFloat)fontSize;
 
 @end
--- UIView+LamAppearence.m
+++ UIView+LamAppearence.m
@@ -20,7 +20,7 @@
 // http://stackoverflow.com/questions/2500915/how-to-create-an-image-from-a-uiview-uiscrollview
 - (UIImage *)generateImage
 {
-    UIGraphicsBeginImageContext(self.frame.size);
+    UIGraphicsBeginImageContextWithOptions(self.frame.size, NO, [UIScreen mainScreen].scale);
     CGContextRef generatingContext = UIGraphicsGetCurrentContext();
     
     [self.layer renderInContext:generatingContext];
@@ -28,6 +28,14 @@
     UIGraphicsEndImageContext();
     
     return newImage;
+}
+
+
+
+// Returns the surface of the view.
+- (CGFloat)surface
+{
+	return self.frame.size.width * self.frame.size.height;
 }
 
 
@@ -49,9 +57,60 @@
 
 
 
+// Add a shadow to the view.
+- (void)addShadowWithOpacity:(CGFloat)anOpacity blur:(CGFloat)aBlurRadius offset:(CGSize)anOffset
+{
+    self.layer.shadowOpacity = anOpacity;
+    self.layer.shadowRadius = aBlurRadius;
+    self.layer.shadowOffset = anOffset;
+}
+
+
+
+// Make a view dance.
+- (void)wiggleViewWithIntensity:(CGFloat)intensity andDuration:(CGFloat)duration
+{
+    CALayer *touchedLayer = [self layer];
+    CABasicAnimation *wiggleAnimation = [CABasicAnimation animationWithKeyPath:@"transform"];
+    wiggleAnimation.duration = duration;
+    wiggleAnimation.repeatCount = MAXFLOAT;
+    wiggleAnimation.autoreverses = YES;
+    wiggleAnimation.fromValue = [NSValue valueWithCATransform3D:
+                                 CATransform3DRotate(touchedLayer.transform, intensity, 0.0, 0.0, 1.0)];
+    wiggleAnimation.toValue = [NSValue valueWithCATransform3D:
+                               CATransform3DRotate(touchedLayer.transform, 0, 0.0, 0.0, 1.0)];
+    [touchedLayer addAnimation:wiggleAnimation forKey:@"wiggle"];
+    
+    CABasicAnimation *moveAnimation = [CABasicAnimation animationWithKeyPath:@"position"];
+    moveAnimation.duration = duration;
+    moveAnimation.repeatCount = MAXFLOAT;
+    moveAnimation.autoreverses = YES;
+    moveAnimation.fromValue = [NSValue valueWithCGPoint:CGPointMake(touchedLayer.position.x,
+                                                                    touchedLayer.position.y)];
+    moveAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(touchedLayer.position.x,
+                                                                  touchedLayer.position.y + 4)];
+    [touchedLayer addAnimation:moveAnimation forKey:@"move"];
+    
+}
+
+
+
 /// Apply the given font to all subviews, but keeps the subview's current font size.
 - (void)setFontForAllSubviewsWithName:(NSString *)aFontName
 {
+	//    // Recursive call to all subviews.
+	//    for (UIView *aView in self.subviews) {
+	//        [aView setFontForAllSubviewsWithName:aFontName];
+	//    }
+	//
+	//    // Do it to myself.
+	//    if ([self respondsToSelector:@selector(setFont:)] &&
+	//        [self respondsToSelector:@selector(font)]) {
+	//
+	//        UIFont *myFont = [(id)self font];
+	//        [(id)self setFont:[UIFont fontWithName:aFontName size:[myFont pointSize]]];
+	//    }
+    
     [self setFontForAllSubviewsWithName:aFontName boldFontName:nil];
 }
 
@@ -74,11 +133,11 @@
         
         UIFont *myFont = [(id)self font];
         NSString *myFontName = [myFont fontName];
-
+		
         // Check if the font is bold and if a bold font has been provided.
         BOOL isBlod = NO;
         if (aBoldFontName != nil) isBlod = isABoldFontName(myFontName);
-                
+		
         UIFont *newFont;
         if (isBlod) newFont = [UIFont fontWithName:aBoldFontName size:[myFont pointSize]];
         else newFont = [UIFont fontWithName:aFontName size:[myFont pointSize]];
@@ -87,7 +146,57 @@
     }
 }
 
+/// Apply the given font to all subviews, but keeps the subview's current font size.
+- (void)setFontForAllSubviewsWithName:(NSString *)aFontName andSize:(CGFloat)fontSize
+{
+	//    // Recursive call to all subviews.
+	//    for (UIView *aView in self.subviews) {
+	//        [aView setFontForAllSubviewsWithName:aFontName];
+	//    }
+	//
+	//    // Do it to myself.
+	//    if ([self respondsToSelector:@selector(setFont:)] &&
+	//        [self respondsToSelector:@selector(font)]) {
+	//
+	//        UIFont *myFont = [(id)self font];
+	//        [(id)self setFont:[UIFont fontWithName:aFontName size:[myFont pointSize]]];
+	//    }
+    
+    [self setFontForAllSubviewsWithName:aFontName boldFontName:nil andSize:fontSize];
+}
 
+
+
+/// Apply the given font to all subviews, but keeps the subview's current font size.
+/// If the original font was bold, boldFontName is used instead.
+- (void)setFontForAllSubviewsWithName:(NSString *)aFontName
+                         boldFontName:(NSString *)aBoldFontName
+							  andSize:(CGFloat)fontSize
+{
+    // Recursive call to all subviews.
+    for (UIView *aView in self.subviews) {
+        [aView setFontForAllSubviewsWithName:aFontName
+                                boldFontName:aBoldFontName];
+    }
+	
+    // Do it to myself.
+    if ([self respondsToSelector:@selector(setFont:)] &&
+        [self respondsToSelector:@selector(font)]) {
+        
+        UIFont *myFont = [(id)self font];
+        NSString *myFontName = [myFont fontName];
+		
+        // Check if the font is bold and if a bold font has been provided.
+        BOOL isBlod = NO;
+        if (aBoldFontName != nil) isBlod = isABoldFontName(myFontName);
+		
+        UIFont *newFont;
+        if (isBlod) newFont = [UIFont fontWithName:aBoldFontName size:fontSize];
+        else newFont = [UIFont fontWithName:aFontName size:fontSize];
+        
+        [(id)self setFont:newFont];
+    }
+}
 
 BOOL isABoldFontName(NSString *fontName)
 {
